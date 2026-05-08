388900827d0fc1942c402dd4aad0a1029d634280
devdsinghh <devworld2006@gmail.com>
2026-05-08 15:23:23 +0530

    Update deoply.sh

--- deoply.sh
+++ deoply.sh
@@ -1,4 +1,4 @@
 docker container rm VivaanXop -f > /dev/null
 sleep 2
 echo "Starting and Deploying MecoMusic"
-docker run -d --restart=always --name VivaanXop VivaanXop
+docker run -d --restart=always --name ziddii ziddiiop
